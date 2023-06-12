092e8e36b14d350de35c52e334b83c0fc8325203
mpcrockett <mpcrockett+github@gmail.com>
2023-06-12 15:22:32 -0400

    client initialization

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,30 +5,28 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")] // GET /api/users
-
-  public class UsersController : ControllerBase
-  {
-    private readonly DataContext _context;
-
-    public UsersController(DataContext context)
+    [ApiController]
+    [Route("api/[controller]")] // GET /api/users
+    public class UsersController : ControllerBase
     {
-      _context = context;
-    }
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
 
-    [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
-    {
-      var users = _context.Users.ToList();
-      return users;
-    }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
 
-    [HttpGet("{id}")]
-    public ActionResult<AppUser> GetUser(int id)
-    {
-      var user = _context.Users.Find(id);
-      return user;
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
     }
-  }
 }
