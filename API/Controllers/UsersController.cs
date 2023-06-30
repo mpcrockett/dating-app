614774a9c2bad75ff0c414a1233dce2b879b4542
mpcrockett <mpcrockett+github@gmail.com>
2023-06-30 13:52:54 -0400

    seeding database and adding automapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,31 +3,37 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using API.Interfaces;
+using AutoMapper;
+using API.DTOs;
 
 namespace API.Controllers
 {
-   // cannot useAnonymous at the highest level and then specify authorization lower down
+   [Authorize]// cannot useAnonymous at the highest level and then specify authorization lower down
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        public IUserRepository _userRepository { get; }
+        private readonly IMapper _mapper;
+        private readonly IMapper mapper;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            this._mapper = mapper;
+            this._userRepository = userRepository;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await _context.Users.ToListAsync();
-            return users;
+            var users = await _userRepository.GetMembersAsync();
+
+            return Ok(users);
         }
 
-        [Authorize]
-        [HttpGet("{id}")]
-
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        [HttpGet("{username}")] 
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _context.Users.FindAsync(id);
+            return await _userRepository.GetMemberAsync(username);
+            
         }
     }
 }
