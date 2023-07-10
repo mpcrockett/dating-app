23f645cea7fd261737b6edacbc20f4cd92f8b808
mpcrockett <mpcrockett+github@gmail.com>
2023-07-10 13:07:01 -0400

    File upload feature for user profile

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditsComponent } from './members/member-edits/member-edits.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     NotFoundComponent,
     ServerErrorComponent,
     MemberCardComponent,
-    MemberEditsComponent
+    MemberEditsComponent,
+    PhotoEditorComponent
   ],
   imports: [
     BrowserModule,
