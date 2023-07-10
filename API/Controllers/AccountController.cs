5bf0f40f77ad3810ea93042868789b76af64ff12
mpcrockett <mpcrockett+github@gmail.com>
2023-07-10 13:57:28 -0400

    navbar photo and change main photo

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -45,7 +45,9 @@ namespace API.Controllers
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-      var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username);
+      var user = await _context.Users
+        .Include(p => p.Photos)
+        .FirstOrDefaultAsync(x => x.UserName == loginDto.Username);
       if (user == null) return Unauthorized("Invalid username.");
 
       using var hmac = new HMACSHA512(user.PasswordSalt);
@@ -60,7 +62,8 @@ namespace API.Controllers
       return new UserDto
       {
         Username = user.UserName,
-        Token = _tokenService.CreateToken(user)
+        Token = _tokenService.CreateToken(user),
+        PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain == true)?.Url
       };
     }
 
--- API/DTOs/PhotoDto.cs
+++ API/DTOs/PhotoDto.cs
@@ -4,7 +4,6 @@ namespace API.DTOs
     {
         public int Id { get; set; }
         public string Url { get; set; }
-
         public bool IsMain { get; set; }
     }
 }
